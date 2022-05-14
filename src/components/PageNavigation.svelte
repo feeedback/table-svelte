c37b96913ff212e179d2c772375aaf5b99decf81
A. Rzhanitsyn <risdeep@gmail.com>
2022-05-14 22:30:33 +0500

    refactor PageNavigation class names

--- src/components/PageNavigation.svelte
+++ src/components/PageNavigation.svelte
@@ -5,30 +5,30 @@
   import { THIN_SPACE } from '../utils/table-utils.js';
 </script>
 
-<div class="pagination__count-page">
+<div class="page-navigation__container">
   <button
-    class="icon-page material-symbols-outlined"
+    class="page-navigation__button material-symbols-outlined"
     disabled={pageNow === 0}
     on:click|capture|stopPropagation={() => {
       pageNow = 0;
     }}>{@html icons.first}</button
   ><button
-    class="icon-page material-symbols-outlined"
+    class="page-navigation__button material-symbols-outlined"
     disabled={pageNow === 0}
     on:click|capture|stopPropagation={() => {
       pageNow -= 1;
     }}>{@html icons.prev}</button
-  ><span class="pagination__count-page-text"
-    >Page: <span class="pagination__count-page-text-current">{counts.page.current}</span
+  ><span class="page-navigation__text"
+    >Page: <span class="page-navigation__count-current-page">{counts.page.current}</span
     >{@html `${THIN_SPACE}/${THIN_SPACE}`}{counts.page.total}</span
   ><button
-    class="icon-page material-symbols-outlined"
+    class="page-navigation__button material-symbols-outlined"
     disabled={pageNow >= counts.page.total - 1}
     on:click|capture|stopPropagation={() => {
       pageNow += 1;
     }}>{@html icons.next}</button
   ><button
-    class="icon-page material-symbols-outlined"
+    class="page-navigation__button material-symbols-outlined"
     disabled={pageNow >= counts.page.total - 1}
     on:click|capture|stopPropagation={() => {
       pageNow = counts.page.total - 1;
@@ -37,23 +37,23 @@
 </div>
 
 <style>
-  button:focus {
+  .page-navigation__container > button:focus {
     border-color: #666;
   }
-  .pagination__count-page {
+  .page-navigation__container {
     display: flex;
     align-items: center;
     column-gap: 7px;
   }
-  .pagination__count-page-text {
+  .page-navigation__text {
     width: 8.5em;
     text-align: center;
     color: #989898;
   }
-  .pagination__count-page-text-current {
+  .page-navigation__count-current-page {
     color: #000;
   }
-  .icon-page {
+  .page-navigation__button {
     font-variation-settings: 'FILL' 0, 'wght' 500, 'GRAD' 0, 'opsz' 40;
     padding: 0;
     margin: 0;
@@ -63,10 +63,10 @@
     user-select: none;
     cursor: pointer;
   }
-  .icon-page:not([disabled]):hover {
+  .page-navigation__button:not([disabled]):hover {
     border-color: rgb(220 220 220);
   }
-  .icon-page:not([disabled]):active {
+  .page-navigation__button:not([disabled]):active {
     color: rgb(0, 0, 255);
     background-color: white;
   }
