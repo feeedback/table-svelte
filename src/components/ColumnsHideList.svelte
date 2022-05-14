1bcf0fce1615fffff0622b844fcb093b6f2d28c3
A. Rzhanitsyn <risdeep@gmail.com>
2022-05-14 22:22:07 +0500

    refactor ColumnsHideList class names

--- src/components/ColumnsHideList.svelte
+++ src/components/ColumnsHideList.svelte
@@ -5,23 +5,25 @@
   export let icons = {};
 </script>
 
-<div class="component-table__columns-list__box">
+<div class="columns-hide-list__container">
   <details>
-    <summary class="columns-list__header">Columns</summary>
-    <div class="columns-list">
+    <summary class="columns-hide-list__header">Columns</summary>
+    <div class="columns-hide-list__list">
       {#each columns as col, i}
-        <div class="columns-list__element">
-          <div class="columns-list__name-box" class:columns-list__name_hidden={hiddenColumns.has(i)}>
-            <span class="columns-list__name" title={col}>{col}</span>
+        <div class="columns-hide-list__list-element">
+          <div class="columns-hide-list__list-element__name-box" class:columns-list__name_hidden={hiddenColumns.has(i)}>
+            <span class="columns-hide-list__list-element__name" title={col}>{col}</span>
           </div>
-          <div class="columns-list__button-box">
+          <div class="columns-hide-list__list-element__button-box">
             {#if hiddenColumns.has(i)}
-              <button class="add-column-button material-symbols-outlined" on:click={() => handlers.addColumn(i)}
-                >{@html icons.add}</button
+              <button
+                class="columns-hide-list__list-element__add-column material-symbols-outlined"
+                on:click={() => handlers.addColumn(i)}>{@html icons.add}</button
               >
             {:else}
-              <button class="hide-column-button material-symbols-outlined" on:click={() => handlers.hideColumn(i)}
-                >{@html icons.hide}</button
+              <button
+                class="columns-hide-list__list-element__hide-column material-symbols-outlined"
+                on:click={() => handlers.hideColumn(i)}>{@html icons.hide}</button
               >
             {/if}
           </div>
@@ -32,7 +34,7 @@
 </div>
 
 <style>
-  .component-table__columns-list__box {
+  .columns-hide-list__container {
     top: 69px;
     left: 18px;
     position: absolute;
@@ -47,7 +49,14 @@
     padding: 7px 8px 0.3em 6px;
     box-shadow: 2px 2px 2px 0px rgb(34 60 80 / 10%);
   }
-  .columns-list {
+  .columns-hide-list__header {
+    user-select: none;
+    cursor: pointer;
+    font-weight: bold;
+    line-height: initial;
+  }
+
+  .columns-hide-list__list {
     width: 11em;
     overflow: hidden;
     padding-left: 1em;
@@ -58,22 +67,17 @@
     margin-top: 1em;
     font-family: 'Roboto';
   }
-  .columns-list > .columns-list__element {
+
+  .columns-hide-list__list-element {
     width: 100%;
     display: flex;
     justify-content: space-between;
   }
-  /* .columns-list > .columns-list__element:not(:last-child) {
+  /* .columns-hide-list__list-element:not(:last-child) {
     margin-bottom: 4px;
   } */
-  .columns-list__header {
-    user-select: none;
-    cursor: pointer;
-    font-weight: bold;
-    line-height: initial;
-  }
 
-  .component-table__columns-list__box .columns-list__element button {
+  .columns-hide-list__list-element button {
     font-variation-settings: 'FILL' 0, 'wght' 500, 'GRAD' 0, 'opsz' 40;
     text-align: center;
     margin: 0;
@@ -86,44 +90,38 @@
     user-select: none;
     cursor: pointer;
   }
-  .component-table__columns-list__box .columns-list__element:hover button {
+  .columns-hide-list__list-element:hover button {
     font-variation-settings: 'FILL' 0, 'wght' 700, 'GRAD' 200, 'opsz' 40;
     /* border: 1px rgb(230, 230, 230) dotted; */
   }
-  .component-table__columns-list__box .columns-list__element:hover button.hide-column-button {
+  .columns-hide-list__list-element:hover button.columns-hide-list__list-element__hide-column {
     font-variation-settings: 'FILL' 0, 'wght' 700, 'GRAD' 200, 'opsz' 40;
   }
-  .component-table__columns-list__box .columns-list__element button:hover {
+  .columns-hide-list__list-element button:hover {
     border-style: solid;
     /* border: 1px rgb(220, 220, 220) dotted; */
   }
-
-  .component-table__columns-list__box .columns-list__element .hide-column-button {
-    /* color: #c85d5d; */
-    /* color: #cd5b5b; */
+  .columns-hide-list__list-element__hide-column {
     color: #df5555;
-    /* font-family: 'Noto Sans Symbols 2', 'Segoe UI Symbol'; */
   }
-  .component-table__columns-list__box .columns-list__element button.hide-column-button:hover {
+  .columns-hide-list__list-element button.columns-hide-list__list-element__hide-column:hover {
     color: rgb(216, 34, 34);
   }
-  .component-table__columns-list__box .columns-list__element .add-column-button {
+  .columns-hide-list__list-element__add-column {
     color: rgb(7, 184, 7);
   }
-  .component-table__columns-list__box .columns-list__element .add-column-button:hover {
+  .columns-hide-list__list-element__add-column:hover {
     color: rgb(0, 153, 0);
   }
 
-  .columns-list__name-box {
+  .columns-hide-list__list-element__name-box {
     width: calc(100% - 3px - 24px);
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
   }
-
   .columns-list__name_hidden {
     text-decoration: line-through;
-    /* color: #989898; */
     color: #b7b7b7;
     text-decoration-thickness: 1px;
     text-decoration-color: rgba(225, 128, 128, 0.486);
